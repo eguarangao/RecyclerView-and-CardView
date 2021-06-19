1767726f9517adbe50c50432d5ff1b0ed940fef1
eguarangao <69706207+eguarangao@users.noreply.github.com>
2021-06-19 00:03:25 -0500

    aggImagenes

--- WEB/app/src/main/java/com/example/uteqlibros/MainActivity.java
+++ WEB/app/src/main/java/com/example/uteqlibros/MainActivity.java
@@ -30,11 +30,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.recyclerview);
         items = new ArrayList<>();
-        date();
-
+        retrofD();
     }
 
-    private void date(){
+    private void retrofD(){
         Retrofit retrofit = new  Retrofit.Builder().baseUrl("https://revistas.uteq.edu.ec/")
                 .addConverterFactory(GsonConverterFactory.create()).build();
         //Se aplica la interfaz
@@ -47,9 +46,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<List<ListaLibrosUteq>> call, Response<List<ListaLibrosUteq>> response) {
                 List<ListaLibrosUteq> LibrosLista = response.body();
                 for(ListaLibrosUteq Lista:LibrosLista){
-                 /*   String responseTest="";
-                    responseTest+=Lista.getIssue_id();
-                    Log.v("Tag",""+responseTest);*/
+
                      items.add(Lista);
                 
                 }
@@ -58,15 +55,15 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<List<ListaLibrosUteq>> call, Throwable t) {
-               System.out.println(t.toString()+"EBERT");
+               System.out.println(t.toString());
             }
         });
     }
 
     private void PutDataIntoRecyclerView(List<ListaLibrosUteq> librosL) {
         AdaptadorRecycler adaptadorRecycler = new AdaptadorRecycler(this, (ArrayList<ListaLibrosUteq>) librosL);
- recyclerView.setLayoutManager(new LinearLayoutManager(this));
- recyclerView.setAdapter(adaptadorRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adaptadorRecycler);
 
     }
 
